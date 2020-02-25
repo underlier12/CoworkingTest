19ed93f205e93ea8355fbd5004f654ead524fef8
Underlier12 <underlier12@gmail.com>
2020-02-26 08:29:59 +0900

    Feat: Test  bed for Branch

--- src/main/java/com/old/websocket/test/Test.java
+++ src/main/java/com/old/websocket/test/Test.java
@@ -2,8 +2,14 @@ package com.old.websocket.test;
 
 public class Test {
 	
+	// method A
 	public void test() {		
 		System.out.println("Test");
 		System.out.println("Plus Test Commit");
+	}
+	
+	// method B
+	public void test2() {
+		//TODO
 	}
 }
