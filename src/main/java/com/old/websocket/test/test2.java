dcf2c1e67f78c026a8be4e944b20118d3ee6b249
hong8159 <hogu8159@naver.com>
2020-02-28 21:41:00 +0900

    feat let'sgo 1 (2)

--- src/main/java/com/old/websocket/test/test2.java
+++ src/main/java/com/old/websocket/test/test2.java
@@ -6,6 +6,7 @@ public class test2 {
 		System.out.println("gogogogo");
 		System.out.println("gogogo different");
 		System.out.println("let's go 1");
+		System.out.println("lets go  1 (2)");
 	}
 	
 
