a101668aee7e42b72765007375b185ede5566a23
hong8159 <hogu8159@naver.com>
2020-02-28 21:12:45 +0900

    feat : he3

--- src/main/java/com/old/websocket/test/test2.java
+++ src/main/java/com/old/websocket/test/test2.java
@@ -3,6 +3,8 @@ package com.old.websocket.test;
 public class test2 {
 	public static void main(String[] args) {
 		System.out.println("he");
+		System.out.println("he2");
+		System.out.println("he3");
 	}
 
 }
