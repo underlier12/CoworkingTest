4c89ef66c53c3c4fca1202c8b7a9073b0dd1691f
hong8159 <hogu8159@naver.com>
2020-02-28 21:31:22 +0900

    commit differer

--- src/main/java/com/old/websocket/test/test2.java
+++ src/main/java/com/old/websocket/test/test2.java
@@ -3,9 +3,7 @@ package com.old.websocket.test;
 public class test2 {
 	public static void main(String[] args) {
 		System.out.println("he");
-		System.out.println("he2");
-		System.out.println("he3");
-		System.out.println("feat2");
+		System.out.println("gogogogo");
 	}
 	
 
